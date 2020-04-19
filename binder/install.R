ee85426f97c2598347bd5e629ab13266469020c8
Samuel Gratzl <sam@sgratzl.com>
2020-04-19 20:08:28 +0200

    different setup

--- binder/install.R
+++
@@ -1,5 +0,0 @@
-# mybinder.org install script
-
-install.packages('devtools')
-install.packages('rmarkdown')
-devtools::install_url("https://github.com/upsetjs/upsetjs_r/releases/latest/download/upsetjs.tar.gz")
